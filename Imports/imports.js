0ab7cb099f2e06ec3ed398d65931928ed1261c4c
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2019-09-09 11:04:03 +0100

    Add imports file

---
+++ Imports/imports.js
@@ -0,0 +1,4 @@
+import Styles from "../src/styles.css";
+import App from "../src/App";
+
+export { App, Styles };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
-import App from "./App";
+import { App, Styles } from "../Imports/imports.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
