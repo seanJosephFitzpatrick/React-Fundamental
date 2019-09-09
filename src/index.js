f267e73c65f119dd04a6e38a68c414c2e98d4361
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2019-09-09 10:58:49 +0100

    Add App.js file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./styles.css";
+import App from "./App";
 
-function App() {
-  return (
-    <div className="App">
-      <a href="https://reactjs.org/docs/introducing-jsx.html">React</a>
-      <h1>This funny tag syntax is neither a string nor HTML.</h1>
-      <p>
-        It is called JSX, and it is a syntax extension to JavaScript. We
-        recommend using it with React to describe what the UI should look like.
-        JSX may remind you of a template language, but it comes the full power
-        of JavaScript.
-      </p>
-    </div>
-  );
-}
-
-/**Rendering an Element into the DOM */
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById("root"));
